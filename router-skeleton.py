779c6aec8dcea36208af513611cc74a2e96298af
John Henry Rudden <johnhenryrudden@johns-mbp.myfiosgateway.com>
2021-10-11 17:08:02 -0400

    handle update and filtering route preferences

--- router-skeleton.py
+++ router-skeleton.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import argparse, socket, time, json, select, struct, math
+import argparse, socket, time, json, select, struct, math, copy
 
 #DEBUG = True
 DEBUG = False
@@ -42,15 +42,36 @@ PROV = "prov"
 
 ##########################################################################################
 
-class Router:
+class Route: 
+    port = None
+    network = None
+    netMask = None
+    localPref = None
+    selfOrigin = None
+    AsPath = None
+    origin = None
+    def __init__(self, port, packet):
+        self.port = port
+        self.network = packet[MESG][NTWK]
+        self.netmask = packet[MESG][NMSK]
+        self.localPref = packet[MESG][LPRF]
+        self.selfOrigin = packet[MESG][SORG]
+        self.AsPath = packet[MESG][APTH]
+        self.origin = packet[MESG][ORIG]
 
+
+## TODO: do we need table for routes
+
+class Router:
+    asn = None
     routes = None
     updates = None
     relations = None
     sockets = None
 
-    def __init__(self, networks):
-        self.routes = []
+    def __init__(self, asn, networks):
+        self.asn = asn
+        self.routes = {} # {port: Route[]}
         self.updates = []
         self.relations = {}
         self.sockets = {}
@@ -62,6 +83,7 @@ class Router:
             self.sockets[network].setblocking(0)
             self.sockets[network].connect(network)
             self.relations[network] = relation
+            self.routes[network] = []
         return
 
     def lookup_routes(self, daddr):
@@ -72,38 +94,78 @@ class Router:
 
     def get_shortest_as_path(self, routes):
         """ select the route with the shortest AS Path """
-        # TODO
-        outroutes = []
-        return outroutes
+        if len(routes) == 0:
+            return []
+        shortest_routes = [routes[0]]
+        shortest_length = routes[0].ASPath.count(",") + 1
+        for route_index in range(1, len(routes)):
+            current_route = routes[route_index]
+            current_length = current_route.ASPath.count(",") + 1
+            if current_length == shortest_length:
+                shortest_routes.append(current_route)
+            elif current_length < shortest_length:
+                shortest_length = current_length
+                shortest_routes = [current_route]
+        return shortest_routes
             
     def get_highest_preference(self, routes):
-        """ select the route with the shortest AS Path """
-        # TODO
-        outroutes = []
-        return outroutes
+        """ select the route with the highest local pref """
+        if len(routes) == 0:
+            return []
+        highest_routes = [routes[0]]
+        highest_pref = routes[0].localPref
+        for route_index in range(1, len(routes)):
+            current_route = routes[route_index]
+            if current_route.localPref == highest_pref:
+                highest_routes.append(current_route)
+            elif current_route.localPref > highest_pref:
+                highest_pref = current_route.localPref
+                highest_routes = [current_route]
+        return highest_routes
          
     def get_self_origin(self, routes):
         """ select self originating routes """
         # TODO
         outroutes = []
+        for route in routes:
+            if route.selfOrigin == "true":
+                outroutes.append(route)
         return outroutes
 
     def get_origin_routes(self, routes):
         """ select origin routes: IGP > EGP > UNK """
         # TODO
         outroutes = []
+        current_priority = "UNK"
+        better = ["EGP", "IGP"]
+        for route in routes:
+            if route.origin == current_priority:
+                outroutes.append(routes)
+            elif route.origin in better:
+                current_priority = route.origin
+                better.remove(route.origin)
+                outroutes = [route]
+
         return outroutes
+        
 
     def filter_relationships(self, srcif, routes):
         """ Don't allow Peer->Peer, Peer->Prov, or Prov->Peer forwards """
         outroutes = []
+        for route in routes:
+            srcif_rel = self.relations[srcif]
+            route_rel = self.relations[route.port]
+            if (srcif_rel == CUST or route_rel == CUST):
+                outroutes.append(route)
+            elif (srcif_rel == PROV and route_rel == PROV):
+                outroutes.append(route)
         return outroutes
 
     def get_route(self, srcif, daddr):
         """	Select the best route for a given address	"""
         # TODO
         peer = None
-        routes = lookup_routers(daddr)
+        routes = self.lookup_routes(daddr)
         # Rules go here
         if routes:
             # 1. Highest Preference
@@ -133,7 +195,18 @@ class Router:
     def update(self, srcif, packet):
         """	handle update packets	"""
         # TODO 
-        return False
+        self.routes[srcif].extend(Route(srcif, packet))
+        packet_copy = copy.deepcopy(packet)
+        packet_copy[MESG][APTH] = f"[{self.asn}, " + packet_copy[MESG][APTH][1:]
+        packet_json = json.dumps(packet_copy)
+        if self.relations[srcif] == CUST:
+            for socket in self.sockets.keys():
+                if socket != srcif:
+                    self.sockets[socket].send(packet_json)
+        else:
+           for socket in self.sockets.keys():
+                if socket != srcif and not(self.relations[socket] in [PROV, PEER]):
+                    self.sockets[socket].send(packet_json) 
     
     def revoke(self, packet):
         """	handle revoke packets	"""
@@ -176,5 +249,5 @@ class Router:
         return
 
 if __name__ == "__main__":
-    router = Router(args.networks)
+    router = Router(args.asn ,args.networks)
     router.run()
